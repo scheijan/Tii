117e4bb2c92c44509a2d642a0ad705657c50fd42
scheijan <scheijan@gmail.com>
2017-04-17 14:50:54 +0200

    create a new game instance for every 'play' request

--- server/src/tiiServer.py
+++ server/src/tiiServer.py
@@ -32,6 +32,7 @@ class TiiServer(object):
     def play(self, p=2, r=20):
         p = int(p)
         r = int(r)
+        self.game = tii.Game(p)
 
         for p in [x for x in self.game.players if not self.game.won]:
             for i in [i for i in range(0, r) if not self.game.won]:
