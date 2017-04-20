8e75493518e5b6fffdfa2463ef97374874e7a5e2
scheijan <scheijan@gmail.com>
2017-04-21 01:42:41 +0200

    adjust cards for the board, get data via ajax call

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -13,15 +13,6 @@ function init() {
       drop: handleDropEvent,
       accept: '.draggable',
     });
-
-
-  $("button").click(function(){
-      $.getJSON("/state", function(result){
-        ('')
-        
-      });
-  });
-
 }
 
 function handleDropEvent( event, ui ) {
@@ -29,4 +20,28 @@ function handleDropEvent( event, ui ) {
   draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
   draggable.draggable( 'option', 'revert', false );
   console.log( 'the card with ID "' + draggable.attr('id') + '" was dropped onto me!' );
+}
+
+function getData(n) {
+  $.getJSON("/state?n=" + n, function(result){
+    var snippet = '<div class="gameinfo">';
+    snippet = snippet + '<div>Name: ' + result.name + '</div>';
+    snippet = snippet + '<div>Round: ' + result.round + '</div>';
+    snippet = snippet + '<div>Draw: ' + result.cardsToDraw + ' / Play:' + result.cardsToPlay + '</div>';
+    snippet = snippet + '</div>'
+    $('#board').prepend(snippet)
+
+    
+    for (var i = 0; i < result.deck._cards.length; i++) {
+      var c = result.deck._cards[i];
+      $('#deck').append('<div class="smallcard draggable" id="' + c.id + '"><img class="smallimg" src="/static/pics/cards/' + c.category + 's/' + c.id + '.png" /></div>')
+    }
+
+    for (var i = 0; i < result.hand._cards.length; i++) {
+      var c = result.hand._cards[i];
+      $('#hand').append('<div class="smallcard draggable" id="' + c.id + '"><img class="smallimg" src="/static/pics/cards/' + c.category + 's/' + c.id + '.png" /></div>')
+    }
+    init();
+  });
+  
 }
