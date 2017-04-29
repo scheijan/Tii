46e14d3cc67c26cb99cb5235c9a0f75d76685e8b
scheijan <scheijan@gmail.com>
2017-04-30 00:47:18 +0200

    show text based cards for rules for now

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -29,18 +29,29 @@ function getData(n) {
     snippet = snippet + '<div>Round: ' + result.round + '</div>';
     
     snippet = snippet + '<div>Draw: ' + result.cardsToDraw + ' / Play:' + result.cardsToPlay + '</div>';
-    $('#gameinfo').empty();
-    $('#gameinfo').prepend(snippet);
+    
+    $('#gameinfo').html(snippet);
 
     if (result.goal) {
-      $('#goalStack').append('<div class="smallcard draggable" id="' + result.goal.id + '"><img class="smallimg" src="/static/pics/cards/goals/' + result.goal.id + '.png" /></div>')
+      $('#goalStack').html('<div class="smallcard draggable" id="' + result.goal.id + '"><img class="smallimg" src="/static/pics/cards/goals/' + result.goal.id + '.png" /></div>')
+    }
+
+
+    $('#rules').empty();
+    for (var i = 0; i < result.rules._cards.length; i++) {
+      var c = result.rules._cards[i];
+
+      $('#rules').append('<div class="smallcard draggable" id="' + c.id + '"><div class="textcard">' + c.name + '</div></div>')
     }
       
+    $('#field').empty();
     for (var i = 0; i < result.field._cards.length; i++) {
       var c = result.field._cards[i];
+
       $('#field').append('<div class="smallcard draggable" id="' + c.id + '"><img class="smallimg" src="/static/pics/cards/' + c.category + 's/' + c.id + '.png" /></div>')
     }
 
+    $('#hand').empty();
     for (var i = 0; i < result.hand._cards.length; i++) {
       var c = result.hand._cards[i];
       $('#hand').append('<div class="smallcard draggable" id="' + c.id + '"><img class="smallimg" src="/static/pics/cards/' + c.category + 's/' + c.id + '.png" /></div>')
